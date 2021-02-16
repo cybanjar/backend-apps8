063c9dd305e1873a37b4841d4461c7ed509d396b
cybanjar <cybanjar@gmail.com>
2021-02-16 11:44:58 +0700

    start base64 image

--- app/Http/Controllers/PostingController.php
+++ app/Http/Controllers/PostingController.php
@@ -6,6 +6,7 @@ use App\Helpers\ResponseFormatter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Posting;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
@@ -21,24 +22,56 @@ class PostingController extends Controller
 
     public function store(Request $request)
     {
+        $posting = auth('api')->user();
+        return response()->json([
+            'success'   => true,
+            'message'   => 'Successfully',
+            'data'      => $posting
+        ]);
+
         $validator = Validator::make($request->all(), [
             'nama'      => 'required|string',
             'harga'     => 'required|integer',
             'deskripsi' => 'required|string',
             'kondisi'   => 'in:baru, bekas',
             'lokasi'    => 'required|string',
-            'picturePath' => 'required|image|max:2048',
+            'picturePath' => 'required|image|max:1024',
         ]);
 
         if ($validator->fails()) {
             return ResponseFormatter::error(['error' => $validator->errors()], 'Posting is Fails', 401);
         }
 
+        $currentPhoto = $posting->picturePath;
+        if ($request->picturePath != $currentPhoto) {
+
+            $name = time() . '.' . explode('/', explode(':', substr($request->picturePath, 0, strpos($request->picturePath, ';')))[1])[1];
+
+            Image::make($request->picturePath)->save(public_path('img/profile/').$name);
+            $request->merge(['picturePath' => $name]);
+
+            $userPhoto = public_path('img/profile/') . $currentPhoto;
+            if (file_exists($userPhoto)) {
+                @unlink($userPhoto);
+            }
+
+            return response()->json([
+                'message' => 'Success!'
+            ], 201);
+        } else {
+            return response()->json(['message' => 'Failed!']);
+        }
+
+        $posting->update($request->all());
+
         $data = $request->all();
-        $data['picturePath'] = $request->file('picturePath')->store('assets/posting', 'public');
+        return ResponseFormatter::success([$data], 'File Successfully uploaded!');
+
+        // $data = $request->all();
+        // $data['picturePath'] = $request->file('picturePath')->store('assets/posting', 'public');
         // $posting = Posting::create($data);
 
-        return ResponseFormatter::success([$data], 'File Successfully uploaded!');
+        // return ResponseFormatter::success([$data], 'File Successfully uploaded!');
 
 
     }
@@ -109,11 +142,11 @@ class PostingController extends Controller
             'file' => 'required|image|max:2048',
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return ResponseFormatter::error(['error' => $validator->errors()], 'Update photo fails', 401);
         }
 
-        if($request->file('file')) {
+        if ($request->file('file')) {
             $file = $request->file->store('assets/user', 'public');
         }
 
